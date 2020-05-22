6ce3d3abc73f7986ab021156fd2819f5b633d742
ChangYuHsuan <changyh1994@gmail.com>
2020-05-22 16:29:59 -0400

    fix a bug

--- movingAverage.m
+++ movingAverage.m
@@ -1,11 +1,11 @@
 function x_avg = movingAverage(x, n)
 %%% Moving average 
 %%% x: vector, n: window size
-    if mod(n,2) == 0
+    if mod(n,2) ~= 0
         n = n-1;
     end
     
-    a = (n-1)/2;
+    a = n/2;
     x_avg = zeros(size(x));
     for i = 1+a:length(x)-a
         x_avg(i) = mean(x(i-a:i+a));
